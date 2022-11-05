ef772f27778354b61335c9d6a4bda464849a01df
Paull20-20 <paulloholiveira77@gmail.com>
2022-11-05 17:49:49 -0300

    feat: create navbar for routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { Link, Outlet } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <nav id='navbar' >
+        <h2>
+          <Link to="/">MoviesLib</Link>
+        </h2>
+        <Link to="/movie/1">Movie</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      <h2>Movies Lib</h2>
+      <Outlet/>
     </div>
   );
 }
