f408ccc8cb34d90b56a4625a0e18c1c811693e0d
Paull20-20 <paulloholiveira77@gmail.com>
2022-11-05 17:50:27 -0300

    feat: calling components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import Search from './pages/Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
