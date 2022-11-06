5c8115f1b8c7868a2ba4ded0d2a029e1b7b316ba
Paull20-20 <paulloholiveira77@gmail.com>
2022-11-05 23:32:26 -0300

    feat: Pág Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+//const moviesUrl = process.env.API;
+//const apikey = process.env.API_KEY;
+
+const moviesUrl = "https://api.themoviedb.org/3/movie/";
+const apikey = "api_key=4c57440a889728bf7ddefe0bc3eb145d";
+
 const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setTopMovies(data.results)
+        //console.log(data)
+    }
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesUrl}top_rated?${apikey}` 
+        console.log(topRatedUrl)
+
+        getTopRatedMovies(topRatedUrl)
+    }, [])
+
+    //console.log(moviesUrl, apikey)
     return (
-        <div>My Home here</div>
+        <div className="container" >
+            <h2 className="title">Melhores Filmes:</h2>
+            <div className="movies-container" >
+                {topMovies.length === 0 && <p>Carregando...</p>}
+                {topMovies && topMovies.map((movie) => 
+                    <MovieCard key={movie.id} movie={movie} />
+                )}
+            </div>
+        </div>
     ) 
 }
 
 export default Home
+
