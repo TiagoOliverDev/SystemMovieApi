32904648136409f15021e82e036d96d547558e5e
Paull20-20 <paulloholiveira77@gmail.com>
2022-11-05 23:32:03 -0300

    feat: Adjust component Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,33 @@
 import { Link } from "react-router-dom"
+import { Input, Space } from 'antd';
+import { BiCameraMovie } from "react-icons/bi"
+
+//import '../../src/style.css'
+import 'antd/dist/antd.min.css'
+
+const { Search } = Input;
+
+const onSearch = (value: string) => console.log(value);
 
 const Navbar = () => {
     return (
      <nav id='navbar'>
         <h2>
-          <Link to="/">MoviesLib</Link>
+          <Link to="/">
+            <BiCameraMovie/> MoviesLib
+          </Link>
         </h2>
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">Search</Link>
+        <form>
+        <Space direction="vertical">
+          <Search
+            placeholder="Busque algum filme..."
+            allowClear
+            enterButton="Buscar"
+            size="large"
+            onSearch={onSearch}
+          />
+        </Space>
+        </form>
       </nav>
     ) 
 }
