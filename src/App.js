1cee6b169e8e04d644b4f4948864f89c402e9bdb
Paull20-20 <paulloholiveira77@gmail.com>
2022-11-05 23:32:54 -0300

    feat: general settings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <h2>Movies Lib</h2>
       <Outlet/>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
 import Movie from './pages/Movie';
 import Search from './pages/Search';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,8 +22,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
